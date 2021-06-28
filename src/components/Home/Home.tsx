d4f9397e810a65897743b1bb2bbb69892ccdb78e
Minh Nguyen <minh.nguyen@hometrack.com.au>
2021-06-28 12:53:36 +1000

    preload

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import loadable from '@loadable/component'
 import { StyledP } from './styles'
 
-const Home = () => (
-  <>
-    <StyledP>This is home page</StyledP>
-    <div><Link to="/about">About</Link></div>
-    <Link to="/contact">Contact</Link>
-  </>
-)
+const IntroLoadable = loadable(() => import('components/Intro'), {
+  fallback: <div>Loading</div>
+})
+
+const Home = () => {
+  const [showIntro, setShowIntro] = useState(false)
+
+  return (
+    <>
+      <StyledP>This is home page</StyledP>
+      <div><Link to="/about">About</Link></div>
+      <Link to="/contact">Contact</Link>
+      <div>
+        <button onClick={() => setShowIntro(true)} onMouseOver={() => IntroLoadable.preload()}>Click me</button>
+        {showIntro && <IntroLoadable />}
+      </div>
+    </>
+  )
+}
+
+
 
 export default Home
---
+++ src/components/Intro/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Intro = () => <p>This is intro section</p>
+
+export default Intro
